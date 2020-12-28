11467fc5d0d16a91cb8b810d84f92ce5e2830089
Jocelyn <72670056+tornozelo@users.noreply.github.com>
2020-12-28 20:52:46 +0000

    Imported react app data from codesandbox

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Hello </h1>
-             <Weather city="Lisbon" />   
-      </header>
       
+             <Weather />   
+   
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,92 @@
 import React from "react";
-import axios from "axios";
- import Loader from "react-loader-spinner";
+import "./Weather.css";
 
-export default function Weather(props) {
+export default function Weather() {
+  let weatherData = {
+    city: "Birmingham",
+    date: "Thursday 20:30",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+    temperature: 10,
+    humidity: 50,
+    wind: 20
+  };
 
-    function handleResponse(response) {
-        alert(`The weather in ${props.city} is ${Math.round(response.data.main.temp)}°C`)
-    }
-    let apiKey = "7f0e26ebcc0056eeecaacb8107edefb1";
-    let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(handleResponse);
-    return (
-         <Loader
-         type="Puff"
-         color="#00BFFF"
-         height={100}
-         width={100}
-         timeout={3000} 
-      />
-    );
+  return (
+    <div className="App">
+      <div className="Weather">
+        <form className="mt-2 mb-3">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city name"
+                className="form-control"
+                autofocus="on"
+                autocomplete="off"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-outline-secondary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <div className="weather-overview">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>
+              <i>
+                Last updated: {weatherData.date}
+                <span />
+              </i>
+            </li>
+            <li>{weatherData.description}</li>
+          </ul>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix current-temperature">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <div className="float-left">
+                <strong>{weatherData.temperature}</strong>
+                <span className="units">
+                  <a href="/">°C</a> | <a href="/">°F</a>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-5 humidity-and-wind">
+            <ul>
+              <li>
+                Humidity: <span>{weatherData.humidity}</span>%
+              </li>
+              <li className="wind-li">
+                Wind: <span>{weatherData.wind}</span> km/h
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row"></div>
+      </div>
+      <small>
+        <a
+          href="https://github.com/jocelyn-allen/vanilla-weather-app"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Open-source code
+        </a>{" "}
+        by Jocelyn Allen
+      </small>
+    </div>
+  );
 }
